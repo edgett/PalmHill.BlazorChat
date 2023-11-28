be2b8a011a0898cfbab5dbe85515230b2984f1e3
Hilimire, Edgett <edgett@pertainium.com>
2023-11-27 19:41:46 -0500

    LLama loading incorrectly

---
+++ PlamHill.BlazorChat/Server/ChatHub.cs
@@ -0,0 +1,41 @@
+﻿using LLama.Common;
+using LLama;
+using Microsoft.AspNetCore.SignalR;
+
+namespace PlamHill.BlazorChat.Server
+{
+    public class ChatHub : Hub
+    {
+
+        LLamaWeights model;
+        ModelParams parameters;
+        public ChatHub(LLamaWeights lLamaWeights, ModelParams modelParams) 
+        {
+            model = lLamaWeights;
+            parameters = modelParams;
+            
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+
+          
+
+            using var context = model.CreateContext(parameters);
+            var ex = new InteractiveExecutor(context);
+            ChatSession session = new ChatSession(ex);
+
+            //// show the prompt
+            //Console.WriteLine();
+            //Console.Write(prompt);
+
+            // run the inference in a loop to chat with LLM
+
+            await foreach (var text in session.ChatAsync(message, new InferenceParams() { Temperature = 0.6f, AntiPrompts = new List<string> { "User:" } }))
+            {
+                await Clients.Caller.SendAsync("ReceiveMessage", user, text);
+            }
+        }
+    }
+
+}
--- PlamHill.BlazorChat/Server/Program.cs
+++ PlamHill.BlazorChat/Server/Program.cs
@@ -1,11 +1,34 @@
+using LLama.Common;
+using LLama;
 using Microsoft.AspNetCore.ResponseCompression;
 using PalmHill.Llama;
+using PlamHill.BlazorChat.Server;
 using System.Runtime.InteropServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+          new[] { "application/octet-stream" });
+});
+//Initlize LLLAMA
+string modelPath = @"C:\Users\localadmin\Downloads\orca-2-13b.Q6_K.gguf"; // change it to your own model path
+                                                                          // Load a model
+ModelParams parameters = new ModelParams(modelPath)
+{
+    ContextSize = 1024,
+    Seed = 1337,
+    GpuLayerCount = 5,
+};
+LLamaWeights model = LLamaWeights.LoadFromFile(parameters);
+
+builder.Services.AddSingleton<LLamaWeights>(model);
+builder.Services.AddSingleton<ModelParams>(parameters);
+
 
 var app = builder.Build();
 
@@ -24,11 +47,16 @@ app.UseHttpsRedirection();
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 
+app.UseResponseCompression();
+
 app.UseRouting();
+
+app.MapHub<ChatHub>("/chathub");
 
 app.MapRazorPages();
 app.MapControllers();
 app.MapFallbackToFile("index.html");
 
-await Class1.DemoChat();
+//Initlize LLLAMA
+
 app.Run();
