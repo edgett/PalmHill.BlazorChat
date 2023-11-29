af3907690e910527a5fd46f7b298d069d774a80e
Hilimire, Edgett <ehilimire@mmitnetwork.com>
2023-11-29 12:12:41 -0500

    Component out stuff

--- PlamHill.BlazorChat/Server/ChatHub.cs
+++ PlamHill.BlazorChat/Server/ChatHub.cs
@@ -16,17 +16,11 @@ namespace PlamHill.BlazorChat.Server
 
         public async Task SendPrompt(string user, string messageId, string message)
         {
-
-
-
             var context = LlamaContext;
             var ex = new InteractiveExecutor(context);
             ChatSession session = new ChatSession(ex);
 
-            //// show the prompt
-            //Console.WriteLine();
-            //Console.Write(prompt);
-
+      
             // run the inference in a loop to chat with LLM
 
             await foreach (var text in session.ChatAsync(message, new InferenceParams() { Temperature = 0.6f, AntiPrompts = new List<string> { "User:" } }))
