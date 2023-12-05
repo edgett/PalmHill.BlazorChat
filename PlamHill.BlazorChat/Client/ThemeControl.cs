0f19f8d1dbc655d078b8beddc8d6c86510249830
Hilimire, Edgett <ehilimire@mmitnetwork.com>
2023-12-05 14:13:13 -0500

    Add theme contol. Simplify ui.

---
+++ PlamHill.BlazorChat/Client/ThemeControl.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components.DesignTokens;
+
+namespace PlamHill.BlazorChat.Client
+{
+    public class ThemeControl
+    {
+
+        public ThemeControl(BaseLayerLuminance baseLayerLuminance, AccentBaseColor accentBaseColor) 
+        { 
+            BaseLayerLuminance = baseLayerLuminance;
+            AccentBaseColor = accentBaseColor;
+        }
+
+        private BaseLayerLuminance? BaseLayerLuminance { get; set; }
+
+        private AccentBaseColor? AccentBaseColor { get; set; }
+
+
+        public async Task ChangeTheme(bool darkMode)
+        {
+
+            if (darkMode)
+            {
+                await BaseLayerLuminance!.WithDefault(0f);
+                await AccentBaseColor!.WithDefault(new Swatch(255, 105, 180));
+
+            }
+            else
+            {
+                await BaseLayerLuminance!.WithDefault(1f);
+                await AccentBaseColor!.WithDefault(new Swatch(255, 105, 180));
+            }
+
+        }
+
+    }
+}
--- PlamHill.BlazorChat/Client/UISettings.cs
+++ PlamHill.BlazorChat/Client/UISettings.cs
@@ -5,5 +5,6 @@ namespace PlamHill.BlazorChat.Client
     public class UISettings : InferenceSettings
     {
         public string SystemMessage { get; set; } = "You are a helpful assistant.";
+        public bool DarkMode { get; set; } = false;
     }
 }
