0c0544b97dae6914dd1634aaca0bc692c314c42b
Hilimire, Edgett <edgett@pertainium.com>
2023-12-04 22:58:37 -0500

    Add system prompt to UI

---
+++ PlamHill.BlazorChat/Client/UISettings.cs
@@ -0,0 +1,9 @@
+﻿using PlamHill.BlazorChat.Shared.Models;
+
+namespace PlamHill.BlazorChat.Client
+{
+    public class UISettings : InferenceSettings
+    {
+        public string SystemMessage { get; set; } = "You are a helpful assistant. Repsond in valid markdown only.";
+    }
+}
