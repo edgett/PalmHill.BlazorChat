b19fcf9c3723245e5824a86bebd111c125f0e196
Hilimire, Edgett <edgett@pertainium.com>
2023-12-09 21:13:59 -0500

    Fix up component wiring

---
+++ PalmHill.BlazorChat/Client/ChatComponents.cs
@@ -0,0 +1,12 @@
+﻿using PalmHill.BlazorChat.Client.Components;
+
+namespace PalmHill.BlazorChat.Client
+{
+    public class ChatComponents
+    {
+        public Chat? ChatRenderer { get; set; }
+        public ChatInput? ChatInput { get; set; }
+        public AttachmentManager? AttachmentManager { get; set; }
+
+    }
+}
