f96f154350ffb7437533ab589c0acaf0373e4c02
Edgett Hilimire <edgett@palmhillai.com>
2024-06-17 23:52:47 -0400

    LlamaKernel setup

--- PalmHill.LlamaKernel/LlamaKernel.cs
+++ PalmHill.LlamaKernel/LlamaKernel.cs
@@ -1,4 +1,15 @@
-﻿using Microsoft.SemanticKernel;
+﻿using LLama;
+using LLamaSharp.KernelMemory;
+using LLamaSharp.SemanticKernel.ChatCompletion;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.KernelMemory;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using PalmHill.Llama.Models;
+using LLamaSharp.SemanticKernel.TextEmbedding;
+using Microsoft.SemanticKernel.Memory;
+using LLama.Common;
+
 
 namespace PalmHill.LlamaKernel
 {
@@ -7,9 +18,25 @@ namespace PalmHill.LlamaKernel
 
         public Kernel Kernel { get; }
 
-        public LlamaKernel()
-        { 
-        
+        public LlamaKernel(InjectedModel injectedModel)
+        {
+            var kernelBuilder = Kernel.CreateBuilder();
+            kernelBuilder.Services.AddSingleton<IChatCompletionService>(sp =>
+            {
+                var chatExecutor = new StatelessExecutor(injectedModel.Model, injectedModel.ModelParams);
+                var llamaSharpChatCompletion = new LLamaSharpChatCompletion(chatExecutor);
+                return llamaSharpChatCompletion;
+            });
+            kernelBuilder.Services.AddKernelMemory(km => {
+                var embedding = new LLamaEmbedder(injectedModel.Model, injectedModel.ModelParams);
+                var textEmbeddingGeneration = new LLamaSharpTextEmbeddingGenerator(embedding);
+                km.WithLLamaSharpTextEmbeddingGeneration(textEmbeddingGeneration);
+                km.Build<MemoryServerless>();
+            });
+
+
+            Kernel = kernelBuilder.Build();
+
         }
     }
 }
